c0f58668d010989b6028771baafdc8f649c21ead
Jose <josefbautista94@gmail.com>
2026-04-15 01:53:39 -0400

    Solved the two sum with map

--- Javascript/Two Sum.js
+++ Javascript/Two Sum.js
@@ -5,21 +5,15 @@
  */
 var twoSum = function(nums, target) {
 
-    let i = 1;
-    let j = 0;
-    
- while( i < nums.length ){
-        if(nums[i] + nums[j] == target){
-            return [i,j]
-        } 
-        else {
-            if(i === nums.length-1){
-                j++;
-                i= j+1;
-            } 
-            else {
-                i++;
-            }
-        }   
-    }   
-};
+    const map = {};
+
+    for(let i = 0; i < nums.length; i++){
+        let current = nums[i];
+        let complement = target - current;
+        if(map[complement] !== undefined){
+            return [map[complement], i];
+        }
+
+        map[current] = i;
+    }
+}
